a0454734f9fd86b9ff24bbf9542d1b3122e38880
Prakash-Arulraj <tp612000@gmail.com>
2023-07-20 19:29:45 +0530

    new

---
+++ src/org/lang/LanguageInfo.java
@@ -0,0 +1,31 @@
+package org.lang;
+//public class child class extends parent class
+public class LanguageInfo extends StateDetails{
+	
+	public void tamilLanguage() {
+		System.out.println("This is tamil language");
+
+	}
+	
+	public void englishLanguage() {
+		System.out.println("This is english language");
+
+	}
+	
+	public void hindiLanguage() {
+		System.out.println("This is hindi language");
+
+	}
+	public static void main (String [] args) {
+		LanguageInfo l = new LanguageInfo();
+		l.tamilLanguage();
+		l.englishLanguage();
+		l.hindiLanguage();
+		
+		//parent class
+		l.southIndia();
+		l.northIndia();
+		
+	}
+
+}
---
+++ src/org/lang/StateDetails.java
@@ -0,0 +1,14 @@
+package org.lang;
+
+public class StateDetails {
+	
+	public void southIndia() {
+		System.out.println("tamil is south india language ");
+
+	}
+	public void northIndia() {
+		System.out.println("hindi is north india language");
+
+	}
+
+}
