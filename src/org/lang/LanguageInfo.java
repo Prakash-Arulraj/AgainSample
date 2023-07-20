91d01dd9a8f1429e66e48d78ae91bbdbdbe1d35f
Prakash-Arulraj <tp612000@gmail.com>
2023-07-20 19:39:55 +0530

    Neww

--- src/org/lang/LanguageInfo.java
+++ src/org/lang/LanguageInfo.java
@@ -2,6 +2,12 @@ package org.lang;
 //public class child class extends parent class
 public class LanguageInfo extends StateDetails{
 	
+	public void arulBranch() {
+		System.out.println("This is Arul Branch");
+
+	}
+	
+	
 	public void tamilLanguage() {
 		System.out.println("This is tamil language");
 
